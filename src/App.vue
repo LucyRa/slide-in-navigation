909565c9a6e104b1d47539c9606c58a6d8cd9533
LucyRa <lucyramplin@gmail.com>
2022-06-30 13:31:46 +0100

    Starting to build and style component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import Navigation from "./components/Navigation.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Navigation />
 </template>
 
 <style>
-@import "./assets/base.css";
-
 #app {
   max-width: 1280px;
   margin: 0 auto;
---
+++ src/components/Navigation.vue
@@ -0,0 +1,53 @@
+<script>
+import "../assets/app.scss";
+import IconMenu from "./icons/IconMenu.vue";
+export default {
+  name: "SlideNavigation",
+  methods: {
+    toggleSlidePanel() {
+      const slidePanel = document.querySelector("div.nav-slide-panel");
+      slidePanel.classList.toggle("open");
+    },
+  },
+  components: { IconMenu },
+};
+</script>
+
+<template>
+  <nav class="slide-in-nav">
+    <div class="nav-bar">
+      <button @click.prevent="toggleSlidePanel">
+        <IconMenu />
+      </button>
+    </div>
+
+    <div class="nav-slide-panel"></div>
+  </nav>
+</template>
+
+<style lang="scss">
+@import "../assets/partials/colours.scss";
+
+nav.slide-in-nav {
+  position: fixed;
+  z-index: 50;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100vw;
+
+  .nav-bar {
+    width: 100%;
+    background-color: $darkBg;
+    padding: 1.25rem;
+    border-radius: 0 0 0.5rem 0.5rem;
+  }
+
+  .nav-slide-panel {
+    position: absolute;
+    right: 0;
+    bottom: 1.25rem;
+    max-width: 20rem;
+  }
+}
+</style>
